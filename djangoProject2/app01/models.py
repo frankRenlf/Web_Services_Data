51d7e1763515879e0fbff649267d6fc179a92f60
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-24 22:44:56 +0800

    update

--- djangoProject2/app01/models.py
+++ djangoProject2/app01/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 
 # Create your models here.
-class UseInfo(models.Model):
+class UserInfo(models.Model):
     username = models.CharField(max_length=32)
     password = models.CharField(max_length=64)
     phone = models.IntegerField()
-    
+
+
+"""
+create 
+"""
