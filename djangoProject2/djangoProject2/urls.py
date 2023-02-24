b08bab6fce7cd8ec2455255e76936174f176c799
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-24 13:42:54 +0800

    update

--- djangoProject2/djangoProject2/urls.py
+++ djangoProject2/djangoProject2/urls.py
@@ -1,4 +1,4 @@
-"""djangoProject URL Configuration
+"""djangoProject2 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+
 from app01 import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('index/', views.index),
+    path('', views.test1),
 ]
