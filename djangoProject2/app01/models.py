0050752789c10f63a3bd26b73f2bc71b076ac83a
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-24 22:38:43 +0800

    Update models.py

--- djangoProject2/app01/models.py
+++ djangoProject2/app01/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class UseInfo(models.Model):
+    username = models.CharField(max_length=32)
+    password = models.CharField(max_length=64)
+    phone = models.IntegerField()
+    
