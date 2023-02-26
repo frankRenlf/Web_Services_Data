b6ea350c89f34b0bd8b1f60525d79c76a4b1c830
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-02-26 10:20:49 +0800

    update

--- EmployeeManagementSystem/app01/views.py
+++ EmployeeManagementSystem/app01/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from app01 import models
 
 
 # Create your views here.
 def depart_list(request):
-    return render(request, 'depart_list.html')
+    depart_list = models.Department.objects.all()
+    return render(request, 'depart_list.html',
+                  {"depart_list": depart_list})
