caab16eb5cf0910a112490b4b0d2c2dc4ca079f6
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-02 13:38:06 +0800

    add middle ware

--- EmployeeManagementSystem/app01/middleware/auth.py
+++ EmployeeManagementSystem/app01/middleware/auth.py
@@ -1,1 +1,10 @@
-from django.middleware.common import CommonMiddleware
+from django.utils.deprecation import MiddlewareMixin
+
+
+class M1(MiddlewareMixin):
+    def process_request(self, request):
+        print("m1 in")
+
+    def process_response(self, request, response):
+        print("m2 out")
+        return response
